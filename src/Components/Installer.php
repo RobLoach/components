10f85d5b0d45837276857a7d481036e304d325d1
Rob Loach <robloach@gmail.com>
2012-10-20 21:19:00 -0400

    Autoloader fixes

--- src/Components/Installer.php
+++ src/Components/Installer.php
@@ -7,10 +7,20 @@ use Composer\Installer\LibraryInstaller;
 class Installer extends LibraryInstaller
 {
     /**
+     * The version of the Components Composer installer.
+     */
+    const VERSION = '@package_version@';
+
+    /**
      * {@inheritDoc}
      */
     public function getInstallPath(PackageInterface $package)
     {
+        // Make sure we're installing the right type of package.
+        if ($package->getType() != 'component') {
+            throw new \InvalidArgumentException('Unable to install packages other than "components".');
+        }
+
         // Retrieve the $name and $vendor names for the package.
         $prettyName = $package->getPrettyName();
         if (strpos($prettyName, '/') !== false) {
@@ -31,15 +41,23 @@ class Installer extends LibraryInstaller
 
         // Copy require.js to the base directory.
         $source = __DIR__.DIRECTORY_SEPARATOR.'Resources'.DIRECTORY_SEPARATOR.'require.js';
-        copy($source, $base.DIRECTORY_SEPARATOR.'autoload.js');
+        copy($source, $base.DIRECTORY_SEPARATOR.'require.js');
 
-        // Write the new autoload.config.js with the new package information.
-        $config = $this->composer->getConfig();
-        $packages = $config->has('component-packages') ? $config->get('component-packages') : array();
-        $packages[] = $this->requireJsPackage($package, $name);
-        $config = $this->requireJsConfig($packages);
-        file_put_contents($base.DIRECTORY_SEPARATOR.'autoload.config.js', $config);
+        // Write the new require.config.js with the new package information.
+        $extra = $this->composer->getPackage()->getExtra();
+        $packages = isset($extra['component-packages']) ? $extra['component-packages'] : array();
+        $requirepackage = $this->requireJsPackage($package, $name);
+        $packages[] = $requirepackage;
+        $javascript = $this->requireJsConfig($packages);
+        file_put_contents($base.DIRECTORY_SEPARATOR.'require.config.js', $javascript);
 
+        // Re-save the array of Component packages for addition later on.
+        $extra['component-packages'][] = $requirepackage;
+        // @todo Save the package to the extra definition so we have a whole
+        // list of needed packages.
+        //$this->composer->getPackage()->setExtra($extra);
+
+        // Instruct to install into the base directory.
         return $base.DIRECTORY_SEPARATOR.$name;
     }
 
@@ -70,19 +88,23 @@ class Installer extends LibraryInstaller
      * From a set of RequireJS packages, construct the JavaScript config.
      */
     protected function requireJsConfig(array $packages) {
-        $json = json_encode($packages);
-        $output = 'var components = {"packages": [' . $json . ']};';
-        $output .= <<<EOT
-        if (typeof require !== "undefined" && require.config) {
-            require.config({packages: components.packages});
-        }
-        else {
-            var require = {packages: components.packages};
-        }
+        // Create the components RequireJS definition.
+        $components['packages'] = $packages;
+        $components['version'] = Installer::VERSION;
+        $json = json_encode($components);
 
-        if (typeof exports !== "undefined" && typeof module !== "undefined") {
-            module.exports = components;
-        }
+        // Construct the JavaScript output.
+        $output = <<<EOT
+var components = $json;
+if (typeof require !== "undefined" && require.config) {
+    require.config({packages: components.packages});
+}
+else {
+    var require = {packages: components.packages};
+}
+if (typeof exports !== "undefined" && typeof module !== "undefined") {
+    module.exports = components;
+}
 EOT;
         return $output;
     }
