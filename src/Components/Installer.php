c8da22aeaa124e0f20384f8f8c2c7b5d6d798e0c
Rob Loach <robloach@gmail.com>
2012-10-20 20:35:29 -0400

    Autoload require

--- src/Components/Installer.php
+++ src/Components/Installer.php
@@ -24,10 +24,21 @@ class Installer extends LibraryInstaller
         $base = 'components';
         if ($this->composer->getPackage()) {
             $extra = $this->composer->getPackage()->getExtra();
-            if (isset($extra['components']['path'])) {
-                $base = $extra['components']['path'];
+            if (isset($extra['components']['install-path'])) {
+                $base = $extra['components']['install-path'];
             }
         }
+
+        // Copy require.js to the base directory.
+        $source = __DIR__.DIRECTORY_SEPARATOR.'Resources'.DIRECTORY_SEPARATOR.'require.js';
+        copy($source, $base.DIRECTORY_SEPARATOR.'autoload.js');
+
+        // Write the new autoload.config.js with the new package information.
+        $config = $this->composer->getConfig();
+        $packages = $config->has('component-packages') ? $config->get('component-packages') : array();
+        $packages[] = $this->requireJsPackage($package, $name);
+        $config = $this->requireJsConfig($packages);
+        file_put_contents($base.DIRECTORY_SEPARATOR.'autoload.config.js', $config);
 
         return $base.DIRECTORY_SEPARATOR.$name;
     }
@@ -38,5 +49,41 @@ class Installer extends LibraryInstaller
     public function supports($packageType)
     {
         return $packageType == 'component';
+    }
+
+    /**
+     * Build a RequireJS package definition from the Composer package.
+     */
+    protected function requireJsPackage(PackageInterface $package, $name) {
+        // The components extra definition replicates what is passed to Require.
+        $extra = $package->getExtra();
+        $output = isset($extra['components']) ? $extra['components'] : array();
+
+        // The location is needed to be passed in so that require knows what
+        // folder to look in.
+        $output['location'] = $name;
+
+        return $output;
+    }
+
+    /**
+     * From a set of RequireJS packages, construct the JavaScript config.
+     */
+    protected function requireJsConfig(array $packages) {
+        $json = json_encode($packages);
+        $output = 'var components = {"packages": [' . $json . ']};';
+        $output .= <<<EOT
+        if (typeof require !== "undefined" && require.config) {
+            require.config({packages: components.packages});
+        }
+        else {
+            var require = {packages: components.packages};
+        }
+
+        if (typeof exports !== "undefined" && typeof module !== "undefined") {
+            module.exports = components;
+        }
+EOT;
+        return $output;
     }
 }
